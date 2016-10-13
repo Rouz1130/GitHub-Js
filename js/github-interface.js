fd7123918b8a8b51fcedd6b29dafef8666fca601
rouz majlessi <rmajlessi@gmail.com>
2016-10-13 16:12:05 -0700

    add jquery in interface JS folder

---
+++ js/github-interface.js
@@ -0,0 +1,11 @@
+var Git = require("./../js/github.js").gitModule;
+
+$(document).ready(function() {
+  $('#getName').click(function() {
+    var name = $('#name').val();
+    $('#name').val("");
+    var gitObject = new Git();
+    gitObject.getGit(name);
+    console.log(gitObject);
+  });
+});
